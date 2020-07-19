55c59e92aeb1b5a88812a94a7312c6cb9aae4932
luhuqiang <luhuqiang@users.noreply.github.com>
2020-07-19 23:38:06 +0800

    Tips

---
+++ mysite/gantt_01/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class Gantt01Config(AppConfig):
+    name = 'gantt_01'
---
+++ mysite/mysite/urls.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('', include('gantt_01.urls', namespace='gantt_01')),    
+    path('admin/', admin.site.urls),
+    path('basic/', include('basic.urls', namespace='basic')),
+    path('sale/', include('sale.urls', namespace='sale')),
+    path('finance/', include('finance.urls', namespace='finance')),
+    path('purchase/', include('purchase.urls', namespace='purchase')),
+    path('inventory/', include('inventory.urls', namespace='inventory')),
+]
